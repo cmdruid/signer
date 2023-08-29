b5ed4a86ee304d557b5ceb42736dfea63c6306ad
cmd <cmd@yggdrasil.onion>
2023-08-29 18:31:03 -0500

    update

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,30 @@
 import { Buff, Bytes } from '@cmdcode/buff-utils'
 
-import { MusigContext, sign } from '@cmdcode/musig2'
+import * as musig from '@cmdcode/musig2'
 
 import {
   hd,
   ecdh,
   hash,
   keys,
-  signer,
-  SignOptions
+  signer
 } from '@cmdcode/crypto-utils'
 
 import * as assert from './assert.js'
 
+type MusigContext = musig.MusigContext
+
+export interface SignOptions {
+  aux     ?: Bytes
+  adaptor ?: string
+  tweak   ?: Bytes
+  throws  ?: boolean
+}
+
 export interface SignerOptions extends SignOptions {
-  hd_code ?: Bytes
-  hd_path ?: string
-  rec_key ?: Bytes
+  hd_code  ?: Bytes
+  hd_path  ?: string
+  recovery ?: Bytes
 }
 
 const MSG_MIN_VALUE = 0xFFn ** 24n
@@ -38,11 +46,11 @@ export class Signer {
     secret  : Bytes,
     options : SignerOptions = {}
   ) {
-    const { hd_path, hd_code, xonly } = options
+    const { hd_path, hd_code } = options
     if (typeof hd_path === 'string') {
       // Derive new key and code from path.
       const { seckey, code } = hd.derive(hd_path, secret, hd_code, true)
-      //
+      // Assert that the secret key exists.
       assert.exists(seckey)
       // Apply new key as secret.
       secret = seckey
@@ -51,16 +59,20 @@ export class Signer {
     }
 
     this._seckey = keys.get_seckey(secret)
-    this._pubkey = keys.get_pubkey(this._seckey, xonly)
+    this._pubkey = keys.get_pubkey(this._seckey, true)
     this._config = options
   }
 
-  _signer (config : SignerOptions) {
-    return (content : Bytes) : Buff => {
-      const msg = Buff.bytes(content)
+  get pubkey () : Buff {
+    return this._pubkey
+  }
+
+  _signer (opt ?: SignOptions) : (msg : Bytes) => Buff {
+    const config = { ...this._config, ...opt }
+    return (msg : Bytes) : Buff => {
       assert.size(msg, 32)
       assert.min_byte_value(msg, MSG_MIN_VALUE)
-      return signer.sign(content, this._seckey, config)
+      return signer.sign(msg, this._seckey, config)
     }
   }
 
@@ -73,14 +85,11 @@ export class Signer {
     return ecdh.get_shared_key(this._seckey, pubkey)
   }
 
-  gen_nonce (
-    message : Bytes,
-    pubkey ?: Bytes
-  ) : Buff {
+  gen_nonce (message : Bytes) : Buff {
     return signer.gen_nonce(
       message,
       this._seckey,
-      pubkey ?? this._pubkey,
+      this._pubkey,
       this._config
     )
   }
@@ -89,8 +98,11 @@ export class Signer {
     return hash.hmac512(this._seckey, message)
   }
 
-  sign (message : Bytes) : Buff {
-    return this._signer(this._config)(message)
+  sign (
+    message  : Bytes,
+    options ?: SignOptions
+  ) : Buff {
+    return this._signer(options)(message)
   }
 
   musign (
@@ -99,10 +111,14 @@ export class Signer {
     aux_data ?: Bytes
   ) : Buff {
     assert.size(sec_nonce, 32)
-    const { challenge, group_pubkey } = context
-    if (aux_data === undefined) aux_data = challenge
-    const int_nonce = this.gen_nonce(aux_data, group_pubkey)
+    const { group_pubkey } = context
+    const int_nonce = this.gen_nonce(aux_data ?? group_pubkey)
     const ext_nonce = Buff.join([ int_nonce, sec_nonce ])
-    return sign.with_ctx(context, this._seckey, ext_nonce)
+    return musig.sign.with_ctx(context, this._seckey, ext_nonce)
+  }
+
+  with (opt : SignOptions) : Signer {
+    const config = { ...this._config, ...opt }
+    return new Signer(this._seckey, config)
   }
 }
