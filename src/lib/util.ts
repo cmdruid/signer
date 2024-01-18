dab4b0fa764ab1ff766d7dcd16097e0d841a0b24
cmd <cmd@yggdrasil.onion>
2024-01-18 14:12:45 -0600

    update

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,9 +1,13 @@
 import { Buff, Bytes } from '@cmdcode/buff'
 import { pkdf256 }     from '@cmdcode/crypto-tools/hash'
 import { get_pubkey }  from '@cmdcode/crypto-tools/keys'
-import { cbc }         from '@noble/ciphers/aes'
 import { HDKey }       from '@scure/bip32'
 import { VERSIONS }    from '../const.js'
+
+import {
+  decrypt_cbc,
+  encrypt_cbc
+} from '@cmdcode/crypto-tools/cipher'
 
 export function parse_extkey (extkey : string) {
   if (extkey.startsWith('xpub')) {
@@ -17,52 +21,28 @@ export function parse_extkey (extkey : string) {
 
 export function derive_secret (
   password : Bytes,
-  pubkey   : Bytes
+  vector   : Bytes
 ) {
-  return pkdf256(password, pubkey, 2048)
+  return pkdf256(password, vector, 2048)
 }
-
 
 export function decrypt_key (
   payload  : Bytes,
   password : Bytes
 ) {
   const bytes  = Buff.bytes(payload)
-  const pubkey = bytes.subarray(0, 32)
-  const data   = bytes.subarray(32)
-  const secret = derive_secret(password, pubkey)
-  return decrypt(data, secret, pubkey)
+  const vector = bytes.subarray(0, 16)
+  const data   = bytes.subarray(16)
+  const secret = derive_secret(password, vector)
+  return decrypt_cbc(data, secret, vector)
 }
 
 export function encrypt_key (
   password : Bytes,
   seckey   : Bytes
 ) {
-  const pubkey  = get_pubkey(seckey)
-  const secret  = derive_secret(password, pubkey)
-  const encdata = encrypt(secret, secret, pubkey)
-  return Buff.join([ pubkey, encdata ])
-}
-
-export function decrypt (
-  payload : Bytes,
-  secret  : Bytes,
-  vector  : Bytes
-) {
-  const dat = Buff.bytes(payload)
-  const sec = Buff.bytes(secret)
-  const vec = Buff.bytes(vector)
-  const dec = cbc(sec, vec).decrypt(dat)
-  return Buff.raw(dec)
-}
-
-export function encrypt (
-  payload : Bytes,
-  secret  : Bytes,
-  vector  : Bytes
-) {
-  const dat = Buff.bytes(payload)
-  const sec = Buff.bytes(secret)
-  const vec = Buff.bytes(vector)
-  return Buff.raw(cbc(sec, vec).encrypt(dat))
+  const vector  = get_pubkey(seckey).slice(0, 16)
+  const secret  = derive_secret(password, vector)
+  const encdata = encrypt_cbc(secret, secret, vector)
+  return Buff.join([ vector, encdata ])
 }
